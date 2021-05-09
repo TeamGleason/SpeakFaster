811e823ee4b0dca1ce8a156d94bc629aa7749087
Jon Campbell <joncamp@microsoft.com>
2021-05-08 21:14:16 -0700

    Update decoder to interpret timestamps
    Tobii Computer Control will insert keys very fast on the user's behalf when doing predictions. We don't truely know when a prediction is used, but we can infer based on the time inbetween keystrokes. Keystrokes that are faster than gaze typing can be safely assumed to be inserted via automated means such as predictions.
    
    Note that certain special keyboard keys, such as delete word, also work in this way. Delete word rapidly inserts ctrl-shift-left-backspace.

--- Observer/SpeakFasterObserver Decoder/SpeakFasterObserver_Decoder.py
+++ Observer/SpeakFasterObserver Decoder/SpeakFasterObserver_Decoder.py
@@ -1,10 +1,27 @@
 import keypresses_pb2
 import sys
+import datetime
 
-# Iterates though all people in the AddressBook and prints info about them.
+minhumantime = datetime.timedelta(milliseconds=60)
+maxdeltatime = datetime.timedelta(days=1)
+
+def datetimeFromTimestamp(timestamp):
+    return datetime.datetime.fromtimestamp(timestamp.seconds + timestamp.nanos/1e9)
+
 def ListKeypresses(keypresses):
-  for keypress in keypresses.keyPresses:
-    print(f"Key:{keypress.KeyPress} Timestamp:{keypress.Timestamp.seconds}.{keypress.Timestamp.nanos}")
+    previousTimestamp = datetime.datetime.min
+
+    for keypress in keypresses.keyPresses:
+        currentTimestamp = datetimeFromTimestamp(keypress.Timestamp)
+        deltaTimestamp = currentTimestamp - previousTimestamp
+
+        isHuman = True
+
+        if deltaTimestamp < minhumantime:
+            isHuman = False
+
+        print(f"Key:{keypress.KeyPress} Timestamp:{keypress.Timestamp.seconds}.{keypress.Timestamp.nanos} Delta:{deltaTimestamp} {isHuman}")
+        previousTimestamp = currentTimestamp
 
 if len(sys.argv) != 2:
   print(f"Usage: {sys.argv[0]} input_file")
@@ -12,7 +29,6 @@ if len(sys.argv) != 2:
 
 keypresses = keypresses_pb2.KeyPresses()
 
-# Read the existing address book.
 filename = sys.argv[1]
 
 f = open(filename, "rb")

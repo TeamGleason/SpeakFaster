fe951caffd80e739d16f0f51b4965adef53c12d3
Shanqing Cai <cais@google.com>
2021-08-07 16:35:06 -0400

    audio_asr.py: Support diarization and infinite streaming (#73)
    * Add speaker-diarization support to script audio_asr.py. See the --speaker_count argument.
    * Make audio_asr.py work for infinitely long recordings (from multiple audio files), so we can work around the duration limit of GCloud Speech-to-Text streaming API.

--- Observer/SpeakFasterObserver Decoder/audio_asr_test.py
+++ Observer/SpeakFasterObserver Decoder/audio_asr_test.py
@@ -40,13 +40,15 @@ class GetConsecutiveAudioFilePathsTest(tf.test.TestCase):
         os.path.join(self.get_temp_dir(), "20210710T120000000-MicWavIn.wav"),
         16000, np.zeros(16000 * 1))
 
-    paths, total_duration_sec = audio_asr.get_consecutive_audio_file_paths(
-        os.path.join(self.get_temp_dir(), "20210710T080000000-MicWavIn.wav"))
-    self.assertEqual(paths, [
+    (path_groups,
+     group_durations_sec) = audio_asr.get_consecutive_audio_file_paths(
         os.path.join(self.get_temp_dir(), "20210710T080000000-MicWavIn.wav"),
-        os.path.join(self.get_temp_dir(), "20210710T080010000-MicWavIn.wav"),
-        os.path.join(self.get_temp_dir(), "20210710T080015000-MicWavIn.wav")])
-    self.assertEqual(total_duration_sec, 10 + 5 + 1)
+        group_limit_sec=15)
+    self.assertEqual(path_groups, [
+        [os.path.join(self.get_temp_dir(), "20210710T080000000-MicWavIn.wav"),
+         os.path.join(self.get_temp_dir(), "20210710T080010000-MicWavIn.wav")],
+        [os.path.join(self.get_temp_dir(), "20210710T080015000-MicWavIn.wav")]])
+    self.assertEqual(group_durations_sec, [10 + 5, 1])
 
   def testGetConsecutiveAudioFilePaths_findsASingleFile(self):
     wavfile.write(
@@ -59,11 +61,12 @@ class GetConsecutiveAudioFilePathsTest(tf.test.TestCase):
         os.path.join(self.get_temp_dir(), "20210710T090000000-MicWavIn.wav"),
         16000, np.zeros(16000 * 5))
 
-    paths, total_duration_sec = audio_asr.get_consecutive_audio_file_paths(
+    (path_groups,
+     group_durations_sec) = audio_asr.get_consecutive_audio_file_paths(
         os.path.join(self.get_temp_dir(), "20210710T080000000-MicWavIn.wav"))
-    self.assertEqual(paths, [
-        os.path.join(self.get_temp_dir(), "20210710T080000000-MicWavIn.wav")])
-    self.assertEqual(total_duration_sec, 10)
+    self.assertEqual(path_groups, [
+        [os.path.join(self.get_temp_dir(), "20210710T080000000-MicWavIn.wav")]])
+    self.assertEqual(group_durations_sec, [10])
 
 
 class LoadAudioDataTest(tf.test.TestCase):
@@ -104,6 +107,114 @@ class AudioDataGeneratorTest(tf.test.TestCase):
         language_code="en-US")
     generator = audio_asr.audio_data_generator(audio_paths, config)
     self.assertLen(list(generator), 2)
+
+
+class RegroupUtterancesTest(tf.test.TestCase):
+
+  def testRegroupOneSpeakerTwoUtterances_notObeyingOriginalBoundary(self):
+    utterances = ["would you like to", "sit down"]
+    words = [
+        ("would", 1, 0.1, 0.2),
+        ("you", 1, 0.2, 0.3),
+        ("like", 1, 0.3, 0.4),
+        ("to", 1, 0.5, 0.6),
+        ("sit", 1, 0.6, 0.7),
+        ("down", 1, 0.7, 0.8)]
+    regrouped = audio_asr.regroup_utterances(utterances, words)
+    self.assertEqual(regrouped, [("would you like to sit down", 1, 0.1, 0.8)])
+
+  def testRegroupOneSpeakerTwoUtterances_obeyingOriginalBoundary(self):
+    utterances = ["would you like to", "sit down"]
+    words = [
+        ("would", 1, 0.1, 0.2),
+        ("you", 1, 0.2, 0.3),
+        ("like", 1, 0.3, 0.4),
+        ("to", 1, 0.5, 0.6),
+        ("sit", 1, 1.6, 1.7),
+        ("down", 1, 1.7, 1.8)]
+    regrouped = audio_asr.regroup_utterances(utterances, words)
+    self.assertEqual(regrouped, [
+        ("would you like to", 1, 0.1, 0.6),
+        ("sit down", 1, 1.6, 1.8)])
+
+  def testRegroupThreeSpeakersOneUtterances(self):
+    utterances = ["would you like to sit down no me neither"]
+    words = [
+        ("would", 1, 0.1, 0.2),
+        ("you", 1, 0.2, 0.3),
+        ("like", 1, 0.3, 0.4),
+        ("to", 1, 0.5, 0.6),
+        ("sit", 1, 0.6, 0.7),
+        ("down", 1, 0.7, 0.8),
+        ("no", 2, 0.8, 0.9),
+        ("me", 3, 0.9, 1.0),
+        ("neither", 3, 1.0, 1.1)]
+    regrouped = audio_asr.regroup_utterances(utterances, words)
+    self.assertEqual(regrouped, [
+        ("would you like to sit down", 1, 0.1, 0.8),
+        ("no", 2, 0.8, 0.9),
+        ("me neither", 3, 0.9, 1.1)])
+
+  def testRegroupTwoSpeakersTwoUtterances(self):
+    utterances = [
+        "would you like to sit down no",
+        "been sitting all day"]
+    words = [
+        ("would", 1, 0.1, 0.2),
+        ("you", 1, 0.2, 0.3),
+        ("like", 1, 0.3, 0.4),
+        ("to", 1, 0.5, 0.6),
+        ("sit", 1, 0.6, 0.7),
+        ("down", 1, 0.7, 0.8),
+        ("no", 2, 0.8, 0.9),
+        ("been", 2, 0.9, 1.0),
+        ("sitting", 2, 1.0, 1.1),
+        ("all", 2, 1.1, 1.2),
+        ("day", 2, 1.2, 1.3)]
+    regrouped = audio_asr.regroup_utterances(utterances, words)
+    self.assertEqual(regrouped, [
+        ("would you like to sit down", 1, 0.1, 0.8),
+        ("no been sitting all day", 2, 0.8, 1.3)])
+
+  def testRegroupTwoSpeakersThreeUtterances(self):
+    utterances = [
+        "would you like to sit down no",
+        "been sitting all day ",
+        "alright"]
+    words = [
+        ("would", 1, 0.1, 0.2),
+        ("you", 1, 0.2, 0.3),
+        ("like", 1, 0.3, 0.4),
+        ("to", 1, 0.5, 0.6),
+        ("sit", 1, 0.6, 0.7),
+        ("down", 1, 0.7, 0.8),
+        ("no", 2, 0.8, 0.9),
+        ("been", 2, 0.9, 1.0),
+        ("sitting", 2, 1.0, 1.1),
+        ("all", 2, 1.1, 1.2),
+        ("day", 2, 1.2, 1.3),
+        ("alright", 1, 1.5, 1.6)]
+    regrouped = audio_asr.regroup_utterances(utterances, words)
+    self.assertEqual(regrouped, [
+        ("would you like to sit down", 1, 0.1, 0.8),
+        ("no been sitting all day", 2, 0.8, 1.3),
+        ("alright", 1, 1.5, 1.6)])
+
+  def testUnmatchedWordsRaisesValuError(self):
+    utterances = ["would you like to", "sit down"]
+    words = [
+        ("would", 1, 0.1, 0.2),
+        ("you", 1, 0.2, 0.3)]
+    with self.assertRaisesRegex(ValueError, r"Some words .* missing"):
+      audio_asr.regroup_utterances(utterances, words)
+
+  def testMismatchInWordsRaisesValueError(self):
+      utterances = [
+          "would you like to sit down no",
+          "been sitting all day "]
+      words = [("would", 1, 0.1, 0.2), ("thou", 1, 0.2, 0.3)]
+      with self.assertRaises(ValueError):
+        audio_asr.regroup_utterances(utterances, words)
 
 
 if __name__ == "__main__":

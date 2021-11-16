453222eb96060f7d7836f6f357761ccba97638c1
Shanqing Cai <cais@google.com>
2021-11-15 22:54:42 -0500

    [Observer] Update data schema version; Enable screenshots by default (#108)
    * [Observer] Update data schema version; Enable screenshots by default

--- Observer/SpeakFasterObserver/Upload.cs
+++ Observer/SpeakFasterObserver/Upload.cs
@@ -11,7 +11,7 @@ namespace SpeakFasterObserver
 {
     internal class Upload
     {
-        private const string SCHEMA_VERSION = "SPO-2105";
+        private const string SCHEMA_VERSION = "SPO-2111";
         private const string BUCKET_NAME = "speak-faster";
 
         private static readonly AmazonS3Client _client;

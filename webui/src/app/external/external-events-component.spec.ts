a9ba2a9caca7d8a640e22a8d5fdad8030e3ab39d
Shanqing Cai <cais@google.com>
2021-12-20 14:53:32 -0500

    Remove cruft

--- webui/src/app/external/external-events-component.spec.ts
+++ webui/src/app/external/external-events-component.spec.ts
@@ -1,5 +1,4 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {By} from '@angular/platform-browser';
 import {Subject} from 'rxjs';
 
 import {InputAbbreviationChangedEvent} from '../types/abbreviation';

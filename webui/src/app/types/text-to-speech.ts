ca6fa8a7d9dd1a77856a348d15f60b999253e919
Shanqing Cai <cais@google.com>
2021-12-31 09:38:43 -0500

    Add more info about volume_gain_db to its doc string

--- webui/src/app/types/text-to-speech.ts
+++ webui/src/app/types/text-to-speech.ts
@@ -2,6 +2,7 @@
 
 export interface TextToSpeechAudioConfig {
   // Volume gain in dB. 0 or undefined corresponds to the original volume (no
-  // gain).
+  // gain). A positive value means a louder than original; a negative value
+  // means softer than original.
   volume_gain_db?: number;
 }

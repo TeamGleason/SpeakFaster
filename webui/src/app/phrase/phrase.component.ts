816caf655ed024fbd68720538e712f983aa1369f
Shanqing Cai <cais@google.com>
2022-01-26 16:56:43 -0500

    [WebUI] Refactor phrase options into PhraseComponent (#162)
    Towards #59 
    
    - `PhraseComponent` is refactored out from `AbbreviationComponent`
    - `PhraseComponent` then can be re-used between different parent components, including `AbbreviationComponent` and soon-to-be-added `QuickPhrasesComponent`
    - Each phrase component contains a phrase option, along with
      two buttons: speak and inject.
    - There is an optional 3rd button: favorite.
    
    See screenshot 1:
    ![image](https://user-images.githubusercontent.com/16824702/151223422-38adeb7a-f26b-48c0-9c74-fe9a421d812a.png)
    
    See screenshot 2:
    ![image](https://user-images.githubusercontent.com/16824702/151223496-02582e2c-2f0e-4d20-9889-19000b28a31d.png)

---
+++ webui/src/app/phrase/phrase.component.ts
@@ -0,0 +1,58 @@
+/** A phrase option for user selection. */
+import {AfterViewInit, Component, ElementRef, EventEmitter, Input, OnDestroy, Output, QueryList, ViewChildren} from '@angular/core';
+import {updateButtonBoxesForElements, updateButtonBoxesToEmpty} from 'src/utils/cefsharp';
+import {createUuid} from 'src/utils/uuid';
+
+@Component({
+  selector: 'app-phrase-component',
+  templateUrl: './phrase.component.html',
+})
+export class PhraseComponent implements AfterViewInit, OnDestroy {
+  private static readonly _NAME = 'PhraseComponent';
+
+  private readonly instanceId = PhraseComponent._NAME + '_' + createUuid();
+  @Input() color: string = '#093F3A';
+  @Input() phraseText!: string;
+  @Input() phraseIndex!: number;
+  @Input() showFavoriteButton: boolean = false;
+  @Output()
+  speakButtonClicked: EventEmitter<{phraseText: string, phraseIndex: number}> =
+      new EventEmitter();
+  @Output()
+  injectButtonClicked: EventEmitter<{phraseText: string, phraseIndex: number}> =
+      new EventEmitter();
+  @Output()
+  favoriteButtonClicked:
+      EventEmitter<{phraseText: string, phraseIndex: number}> =
+          new EventEmitter();
+
+  @ViewChildren('clickableButton')
+  clickableButtons!: QueryList<ElementRef<HTMLElement>>;
+
+  ngAfterViewInit() {
+    updateButtonBoxesForElements(this.instanceId, this.clickableButtons);
+    this.clickableButtons.changes.subscribe(
+        (queryList: QueryList<ElementRef>) => {
+          updateButtonBoxesForElements(this.instanceId, queryList);
+        });
+  }
+
+  ngOnDestroy() {
+    updateButtonBoxesToEmpty(this.instanceId);
+  }
+
+  onSpeakButtonClicked(event: Event) {
+    this.speakButtonClicked.emit(
+        {phraseText: this.phraseText, phraseIndex: this.phraseIndex});
+  }
+
+  onInjectButtonClicked(event: Event) {
+    this.injectButtonClicked.emit(
+        {phraseText: this.phraseText, phraseIndex: this.phraseIndex});
+  }
+
+  onFavoriteButtonClicked(event: Event) {
+    this.favoriteButtonClicked.emit(
+        {phraseText: this.phraseText, phraseIndex: this.phraseIndex});
+  }
+}

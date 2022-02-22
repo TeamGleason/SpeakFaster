e1fe80be56a8261929ec8ca86945a62e794b19a5
Shanqing Cai <cais@google.com>
2022-02-21 22:09:59 -0500

    [WebUI] Refactor TextToSpeechComponent to support local TTS (#207)
    - Support both cloud TTS (ttsVoiceType === 'PERSONALIZED') and generic local TTS (ttsVoiceType === 'GENERIC')
    - In TextToSpeechComponent, remove the constituent UI elements for status display (e.g., playing, error). Instead, delegate the status display to listeners.
    
    Fixes #198

--- webui/src/app/text-to-speech/text-to-speech.component.ts
+++ webui/src/app/text-to-speech/text-to-speech.component.ts
@@ -3,11 +3,48 @@ import {HttpErrorResponse} from '@angular/common/http';
 import {ChangeDetectorRef, Component, ElementRef, Input, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {Subject} from 'rxjs';
 
+import {AppSettings, getAppSettings} from '../settings/settings';
 import {TextToSpeechErrorResponse, TextToSpeechService} from '../text-to-speech-service';
 import {TextEntryEndEvent} from '../types/text-entry';
 
 const DEFAULT_LANGUAGE_CODE = 'en-US';
 const DEFAULT_AUDIO_ENCODING = 'LINEAR16';
+
+export type TextToSpeechState = 'REQUESTING'|'PLAY'|'END'|'ERROR';
+
+export interface TextToSpeechEvent {
+  state: TextToSpeechState;
+
+  errorMessage?: string;
+}
+
+export type TextToSpeechListener = (event: TextToSpeechEvent) => void;
+
+export function getCloudTextToSpeechVolumeGainDb(appSettings: AppSettings):
+    number {
+  // Unit: dB. Default is 0.
+  const volume = appSettings.ttsVolume;
+  switch (volume) {
+    case 'QUIET':
+      return -10.0;
+    case 'MEDIUM':
+      return 0.0;
+    case 'LOUD':
+      return 16.0;
+  }
+}
+
+export function getLocalTextToSpeechVolume(appSettings: AppSettings): number {
+  const volume = appSettings.ttsVolume;
+  switch (volume) {
+    case 'QUIET':
+      return 0.2;
+    case 'MEDIUM':
+      return 0.5;
+    case 'LOUD':
+      return 1.0;
+  }
+}
 
 @Component({
   selector: 'app-text-to-speech-component',
@@ -15,39 +52,65 @@ const DEFAULT_AUDIO_ENCODING = 'LINEAR16';
   providers: [TextToSpeechService],
 })
 export class TextToSpeechComponent implements OnInit {
+  private static readonly listeners: TextToSpeechListener[] = [];
+
   @Input() textEntryEndSubject!: Subject<TextEntryEndEvent>;
   @Input() accessToken!: string;
 
   @ViewChildren('ttsAudio')
   ttsAudioElements!: QueryList<ElementRef<HTMLAudioElement>>;
-
-  errorMessage?: string|null = null;
-  audioPlaying: boolean = false;
+  private _audioPlayCallCount: number = 0;
+  private audioPlayDisabledForTest = false;
 
   constructor(
       public textToSpeechService: TextToSpeechService,
       private cdr: ChangeDetectorRef) {}
 
+  public static registerTextToSpeechListener(listener: TextToSpeechListener) {
+    if (TextToSpeechComponent.listeners.indexOf(listener) !== -1) {
+      return;
+    }
+    TextToSpeechComponent.listeners.push(listener);
+  }
+
+  public static unregisterTextToSpeechListener(listener: TextToSpeechListener) {
+    const index = TextToSpeechComponent.listeners.indexOf(listener);
+    if (index === -1) {
+      return;
+    }
+    TextToSpeechComponent.listeners.splice(index, 1);
+  }
+
+  public static clearTextToSpeechListener() {
+    TextToSpeechComponent.listeners.splice(0);
+  }
+
   ngOnInit() {
-    this.textEntryEndSubject.subscribe(event => {
-      // TODO(cais): Add unit test.
+    this.textEntryEndSubject.subscribe(async event => {
       if (!event.isFinal || event.inAppTextToSpeechAudioConfig === undefined) {
         return;
       }
-      const {volume_gain_db} = event.inAppTextToSpeechAudioConfig;
-      if (volume_gain_db !== undefined && volume_gain_db !== 0) {
-        // TODO(#49): Support volume control.
-        throw new Error('Volume gain adjustment is not implemented yet.');
+      const appSettings = await getAppSettings();
+      const ttsVoiceType = appSettings.ttsVoiceType;
+      if (ttsVoiceType === 'PERSONALIZED') {
+        this.doCloudTextToSpeech(event.text, appSettings);
+      } else {
+        this.doLocalTextToSpeech(event.text, appSettings);
       }
-      this.sendTextToSpeechRequest(event.text);
     });
   }
 
-  sendTextToSpeechRequest(text: string) {
+  /**
+   * Send Cloud call for speech synthesis and then play the synthesized audio.
+   */
+  private doCloudTextToSpeech(text: string, appSettings: AppSettings) {
     if (this.accessToken.length === 0) {
-      this.errorMessage = 'no access token';
+      TextToSpeechComponent.listeners.forEach(listener => {
+        listener({state: 'ERROR', errorMessage: 'No access token'});
+      });
       return;
     }
+    this.setListenersState('REQUESTING');
     this.textToSpeechService
         .synthesizeSpeech({
           text,
@@ -55,39 +118,78 @@ export class TextToSpeechComponent implements OnInit {
           audio_config: {
             audio_encoding: DEFAULT_AUDIO_ENCODING,
             speaking_rate: 1.0,
+            volume_gain_db: getCloudTextToSpeechVolumeGainDb(appSettings),
           },
           access_token: this.accessToken,
         })
         .subscribe(
             data => {
-              this.errorMessage = null;
-              const ttsAudioElement = this.ttsAudioElements.first.nativeElement;
+              const ttsAudioElement =
+                  this.ttsAudioElements.first.nativeElement as HTMLAudioElement;
               if (!ttsAudioElement.onplay) {
                 ttsAudioElement.onplay = () => {
-                  this.audioPlaying = true;
+                  this.setListenersState('PLAY');
                 };
                 ttsAudioElement.onended = () => {
-                  this.audioPlaying = false;
+                  this.setListenersState('END');
                 };
               }
               if (!data.audio_content) {
-                this.errorMessage = 'audio is empty';
+                TextToSpeechComponent.listeners.forEach(listener => {
+                  listener({state: 'ERROR', errorMessage: 'Audio is empty'});
+                });
+                ;
                 return;
               }
               ttsAudioElement.src =
                   'data:audio/wav;base64,' + data.audio_content;
-              ttsAudioElement.play();
+              if (!this.audioPlayDisabledForTest) {
+                ttsAudioElement.play();
+              }
+              this._audioPlayCallCount++;
               this.cdr.detectChanges();
             },
             (error: HttpErrorResponse) => {
+              let errorMessage = '';
               if (error.error &&
                   (error.error as TextToSpeechErrorResponse).error_message) {
-                this.errorMessage =
+                errorMessage =
                     (error.error as TextToSpeechErrorResponse).error_message;
               } else {
-                this.errorMessage = `${error.statusText}`;
+                errorMessage = `${error.statusText}`;
               }
+              TextToSpeechComponent.listeners.forEach(listener => {
+                listener({state: 'ERROR', errorMessage});
+              });
               this.cdr.detectChanges();
             });
+  }
+
+  /** Use local WebSpeech API to perform text-to-speech output. */
+  private doLocalTextToSpeech(text: string, appSettings: AppSettings) {
+    const utterance = new SpeechSynthesisUtterance(text.trim());
+    this.setListenersState('REQUESTING');
+    utterance.onstart = () => {
+      this.setListenersState('PLAY');
+    };
+    utterance.onend = () => {
+      this.setListenersState('END');
+    };
+    utterance.volume = getLocalTextToSpeechVolume(appSettings);
+    window.speechSynthesis.speak(utterance);
+  }
+
+  private setListenersState(state: TextToSpeechState, errorMessage?: string) {
+    TextToSpeechComponent.listeners.forEach(listener => {
+      listener({state, errorMessage});
+    });
+  }
+
+  public disableAudioElementPlayForTest() {
+    this.audioPlayDisabledForTest = true;
+  }
+
+  get audioPlayCallCount(): number {
+    return this._audioPlayCallCount;
   }
 }

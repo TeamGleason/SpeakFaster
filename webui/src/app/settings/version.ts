54645a4dfd8a4608a40904d77505e2d95ad5f665
Shanqing Cai <cais@google.com>
2022-05-10 12:14:25 -0400

    [WebUI] Bump version to 0.0.7 (#316)

--- webui/src/app/settings/version.ts
+++ webui/src/app/settings/version.ts
@@ -2,7 +2,7 @@
 
 const MAJOR_VERSION: string = '0';
 const MINOR_VERSION: string = '0';
-const PATCH_VERSION: string = '6';
+const PATCH_VERSION: string = '7';
 
 export const VERSION: string =
     `${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}`;
